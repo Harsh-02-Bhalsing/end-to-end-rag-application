faf5f0e3daecc8e55f0097b02cdc8a6b4f18782e
Harsh-02-Bhalsing <bhalsingharshad@gmail.com>
2026-01-16 13:25:49 +0530

    changed embedding model

--- backend/core/embedding.py
+++ backend/core/embedding.py
@@ -1,12 +1,12 @@
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
-#from langchain_huggingface.embeddings import HuggingFaceEmbeddings
-from core.keys import GEMINI_API_KEY
+# from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_huggingface.embeddings import HuggingFaceEmbeddings
+# from core.keys import GEMINI_API_KEY
 
-embeddings = GoogleGenerativeAIEmbeddings(
-    api_key=GEMINI_API_KEY,
-    model="gemini-embedding-001"
-)
+# embeddings = GoogleGenerativeAIEmbeddings(
+#     api_key=GEMINI_API_KEY,
+#     model="gemini-embedding-001"
+# )
 
-#embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L12-v2")
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L12-v2")
 
 #backup model =sentence-transformers/all-MiniLM-L12-v2 ,  sentence-transformers/all-mpnet-base-v2 , sentence-transformers/all-MiniLM-L6-v2
